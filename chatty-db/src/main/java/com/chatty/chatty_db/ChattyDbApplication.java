f8c7a0c7577fb7aa87155fbe2c0d555966d73061
cjmckernan <conorjmckernan@gmail.com>
2024-08-09 19:31:53 +0100

    added project for database processing

---
+++ chatty-db/src/main/java/com/chatty/chatty_db/ChattyDbApplication.java
@@ -0,0 +1,12 @@
+package com.chatty.chatty_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattyDbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChattyDbApplication.class, args);
+  }
+}
---
+++ chatty-db/src/test/java/com/chatty/chatty_db/ChattyDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatty.chatty_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattyDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
